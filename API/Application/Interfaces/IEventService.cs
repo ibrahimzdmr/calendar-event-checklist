1cb7f940b28bb4a9bbb50276688f23d09c77dc77
Ibrahim Ozdemir <ibrahim.ozdemir@outlook.com.tr>
2022-11-08 19:43:31 +0300

    Application added and first service with dbcontext

---
+++ API/Application/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using DataLayer.Entities;
+using DataLayer.Models;
+
+namespace Application.Interfaces
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<EventItem>> GetEventItemsAsync(CancellationToken cancellationToken);
+        Task CreateEventAsync(EventCreateRequest request, CancellationToken cancellationToken);
+        Task DeleteEventAsync(Guid eventId, CancellationToken cancellationToken);
+        Task UpdateEventAsync(EventItem eventItem, CancellationToken cancellationToken);
+    }
+}
---
+++ API/Application/Services/EventService.cs
@@ -0,0 +1,52 @@
+﻿using API.Data;
+using Application.Interfaces;
+using AutoMapper;
+using DataLayer.Entities;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services
+{
+    public class EventService : IEventService
+    {
+        readonly AppDbContext _context;
+        readonly IMapper _mapper;
+        public EventService(AppDbContext context, IMapper mapper) 
+        {
+            _context = context;
+            _mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<EventCreateRequest, EventItem>();
+            }).CreateMapper(); ;
+        }
+
+        public async Task<IEnumerable<EventItem>> GetEventItemsAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Set<EventItem>().AsNoTracking().Where(i => i.Status).ToListAsync(cancellationToken);
+        }
+
+        public async Task CreateEventAsync(EventCreateRequest request, CancellationToken cancellationToken)
+        {
+            var newEvent = _mapper.Map<EventItem>(request);
+            await _context.Set<EventItem>().AddAsync(newEvent);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEventAsync(Guid eventId, CancellationToken cancellationToken)
+        {
+            var eventItem = await _context.Set<EventItem>().FirstOrDefaultAsync(i => i.Id == eventId, cancellationToken);
+            if (eventItem == null)
+                throw new NullReferenceException();
+
+            eventItem.Status = false;
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateEventAsync(EventItem eventItem, CancellationToken cancellationToken)
+        {
+
+        }
+
+
+    }
+}
